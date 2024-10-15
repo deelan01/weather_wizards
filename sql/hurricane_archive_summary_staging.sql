2131e556c95a59ed99e6b834814d6890bf4bceab
jhammans <hef1125@hotmail.com>
2024-10-15 00:41:59 -0500

    Updated sql files and postgres notebook

--- sql/hurricane_archive_summary_staging.sql
+++ sql/hurricane_archive_summary_staging.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS public.hurricane_archive_summary_staging
     year integer,
     storms integer,
     hurricanes integer,
-    deaths integer,
-    damage_millions_usd integer,
+    deaths varchar(25),
+    damage_millions_usd varchar(25),
     retired_names character varying(100) 
 );
